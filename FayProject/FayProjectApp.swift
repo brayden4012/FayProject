bb7b93840f82e4c563f1b4b65618820b8963b4b4
Brayden Harris <brayden4012@gmail.com>
2025-06-25 16:57:32 -0500

    Initial Commit

---
+++ FayProject/FayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayProjectApp.swift
+//  FayProject
+//
+//  Created by Brayden Harris on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FayProjectTests/FayProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FayProjectTests.swift
+//  FayProjectTests
+//
+//  Created by Brayden Harris on 6/25/25.
+//
+
+import Testing
+@testable import FayProject
+
+struct FayProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
